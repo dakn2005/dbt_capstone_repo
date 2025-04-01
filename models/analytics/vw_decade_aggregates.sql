11b55c80327fc852be5aebc373f7701ee0c40e94
Antony N <techserveservicesltd@gmail.com>
2025-04-01 10:45:27 +0000

    calculating Year-of-Year, Decade-over-decade rates

--- models/analytics/vw_decade_aggregates.sql
+++ models/analytics/vw_decade_aggregates.sql
@@ -1,43 +1,43 @@
 with 
 d_1920s as (
-    select '1920s' decade, sum(cnt) incidents_cnt from {{ ref('vw_yearly_incidents') }}
+    select '1920s' decade, 1920 year_start, sum(cnt) incidents_cnt from {{ ref('vw_yearly_incidents') }}
     where year between 1920 and 1929
 ),d_1930s as (
-    select '1930s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '1930s', 1930, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 1930 and 1939
 ),d_1940s as (
-    select '1940s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '1940s', 1940,sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 1940 and 1949
 ),d_1950s as (
-    select '1950s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '1950s', 1950, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 1950 and 1959
 ),
 d_1960s as (
-    select '1960s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '1960s', 1960, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 1960 and 1969
 ),
 d_1970s as (
-    select '1970s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '1970s', 1970, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 1970 and 1979
 ),
 d_1980s as(
-    select '1980s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '1980s', 1980, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 1980 and 1989
 ),
 d_1990s as (
-    select '1990s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '1990s', 1990, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 1990 and 1999
 ),
 d_2000s as (
-    select '2000s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '2000s', 2000, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 2000 and 2009
 ),
 d_2010s as (
-    select '2010s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '2010s', 2010, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 2010 and 2019
 ),
 d_2020s as (
-    select '2020s', sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
+    select '2020s', 2020, sum(cnt) cnt from {{ ref('vw_yearly_incidents') }}
     where year between 2020 and 2029
 )
 select * from d_1920s
--- models/analytics/vw_manufacturer_distribution.sql
+++
@@ -1,6 +0,0 @@
-select 
-    manufacturer,
-    count(manufacturer) cnt
-from 
-{{ ref('fct_incident_data') }}
-group by manufacturer
